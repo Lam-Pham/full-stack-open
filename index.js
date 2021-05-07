e97f17871376d93e288ceb654888a17b7c205392
Lam-Pham <lpham127@gmail.com>
2021-05-07 15:55:31 -0700

    exercise 3.1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "562-123-4561",
+      },
+      {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "626-998-7546"
+      },
+      {
+        id: 3,
+        name: "Dan Abramov",
+        number: "310-876-1242"
+      },
+      {
+        id: 4,
+        name: "Mary Poppendist",
+        number: "714-412-3874"
+      }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
